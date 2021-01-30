7b8d16979725bbe278e7b3adf242ea5605476b30
Brian Zimmerman <briangzimmermam@gmail.com>
2021-01-30 11:22:54 -0600

    Added script

---
+++ main.js
@@ -0,0 +1,56 @@
+const got                   = require('got');
+const { createWriteStream } = require("fs");
+const { spawn }             = require("child_process");
+
+const downloadUrl        = 'https://www.privateinternetaccess.com/installer/x/download_installer_linux';
+const urlRegex           = new RegExp('url=(https:\/\/.*\.run)');
+const scriptFile         = '/tmp/pia.run';
+const scriptWriterStream = createWriteStream(scriptFile);
+
+// Create stream to write update script to. Run the script when done.
+scriptWriterStream
+.on('error', error => {
+    console.log('Could not download script:', error);
+})
+.on('finish', () => {
+    console.log('Script downloaded to:', scriptFile);
+    console.log("Running script...\n");
+
+    let shell = spawn('sh', [scriptFile], { stdio: 'inherit' });
+
+    shell.on('error', (error) => {
+        console.log(`\nerror: ${error.message}`);
+    }).on("close", code => {
+        console.log(`\nchild process exited with code ${code}`);
+    });
+});
+
+// Function to get the script download url from the html
+let getDownloadUrl = function (downloadPageHtml)
+{
+    let regexMatches = urlRegex.exec(downloadPageHtml);
+    
+    return regexMatches[1] || false;
+};
+
+// Grab the html from the download page and download file
+got(downloadUrl)
+.then(response => {
+    let url = getDownloadUrl(response.body);
+
+    if (!url) {
+        throw new Error('Could not find script url.');
+    }
+
+    return url;
+})
+.then(scriptDownloadUrl => {
+    console.log('Found script url:', scriptDownloadUrl);
+    console.log('Downloading script...');
+
+    let downloadStream = got.stream(scriptDownloadUrl);
+    downloadStream.pipe(scriptWriterStream);
+})
+.catch(error => {
+    console.log(error);
+});
